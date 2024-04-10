a0d9029758f4b3037b7eca3e4fdd10f2e271a385
Maxim Kulkin <maxim.kulkin@gmail.com>
2024-04-10 09:48:26 -0400

    Configure some keymaps for LeetCode mode

--- nvim/lua/config/leetcode.lua
+++ nvim/lua/config/leetcode.lua
@@ -1,3 +1,42 @@
 require('leetcode').setup {
   lang = 'cpp',
+  logging = true,
+  injector = {
+    cpp = {
+      before = {
+        "#include <string>",
+        "#include <vector>",
+        "using namespace std;",
+      },
+      after = {
+        "int main() { return 0; }",
+      }
+    },
+  },
+  hooks = {
+    LeetEnter = {
+      function()
+        vim.cmd([[NvimTreeClose]])
+
+        vim.keymap.set('n', '<leader>lm', ':Leet menu<CR>')
+        vim.keymap.set('n', '<leader>ll', ':Leet list<CR>')
+        vim.keymap.set('n', '<leader>ld', ':Leet desc<CR>')
+        vim.keymap.set('n', '<leader>lc', ':Leet console<CR>')
+        vim.keymap.set('n', '<leader>lr', ':Leet run<CR>')
+        vim.keymap.set('n', '<leader>ls', ':Leet submit<CR>')
+        require('which-key').register({
+          ['<leader>l'] = {
+            name = 'Leetcoode',
+            m = 'Leetcode Menu',
+            l = 'Leetcode List',
+            d = 'Leetcode Description',
+            c = 'Leetcode Console',
+            r = 'Leetcode Run',
+            s = 'Leetcode Submit',
+          },
+        })
+      end,
+    },
+  },
 }
+
