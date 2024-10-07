9cbd715847f9390bb147f31a4b8a7a86ae86b0f9
Maxim Kulkin <maxim.kulkin@gmail.com>
2024-10-06 21:14:17 -0400

    Update config to load Mason plugin always (not lazy)

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -102,7 +102,6 @@ require('packer').startup(function(use)
   }
 
   use { 'williamboman/mason.nvim',
-    cmd = { 'Mason', 'MasonInstall', 'MasonInstallAll', 'MasonUninstall', 'MasonUninstallAll', 'MasonLog' },
     config = function()
       require('config.mason')
     end,
