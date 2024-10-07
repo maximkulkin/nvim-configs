7884dcda0141b5a9097878ed0ed6a6838b77280d
Maxim Kulkin <maxim.kulkin@gmail.com>
2024-10-06 21:01:11 -0400

    Enable python provider

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = '\\'
 -- Disable default providers
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
-vim.g.loaded_python3_provider = 0
+-- vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 vim.opt.colorcolumn = "85"
