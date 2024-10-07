209570ee8adc0567d6696760352ca0ba92921470
Maxim Kulkin <maxim.kulkin@gmail.com>
2024-10-06 21:06:47 -0400

    Add cmp plugins and config

---
+++ nvim/lua/config/cmp.lua
@@ -0,0 +1,78 @@
+local cmp = require('cmp')
+
+local function if_cmp(fn, ...)
+  return cmp.mapping(function(fallback)
+    if cmp.visible() then
+      fn(unpack(arg))
+    else
+      fallback()
+    end
+  end, {'i', 'c'})
+end
+
+cmp.setup {
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  },
+  enabled = true,
+  completion = {
+    keyword_length = 3,
+  },
+  preselect = cmp.PreselectMode.None,
+  view = {
+    docs = {
+      auto_open = true,
+    },
+  },
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = true,
+  },
+  mapping = {
+    ['<C-k>'] = if_cmp(cmp.select_prev_item),
+    ['<C-j>'] = if_cmp(cmp.select_next_item),
+    ['<C-b>'] = if_cmp(cmp.scroll_docs, -4),
+    ['<C-f>'] = if_cmp(cmp.scroll_docs, 4),
+    ['<C-h>'] = if_cmp(cmp.abort),
+    ['<C-l>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.confirm()
+      else
+        cmp.complete()
+      end
+    end, {'i', 'c'}),
+    ['<C-g>'] = function()
+      if cmp.visible_docs() then
+        cmp.close_docs()
+      else
+        cmp.open_docs()
+      end
+    end,
+  },
+}
+
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' },
+  },
+})
+
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources(
+    {
+      { name = 'path' },
+    },
+    {
+      { name = 'cmdline' },
+    }
+  ),
+})
+
+cmp.setup.filetype('neorepl', {
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  },
+})
+
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -122,6 +122,16 @@ require('packer').startup(function(use)
     end,
   }
 
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use { 'hrsh7th/nvim-cmp',
+    config = function()
+      require('config.cmp')
+    end,
+  }
+
   use { 'AckslD/nvim-neoclip.lua',
     config = function()
       require('config.neoclip')
