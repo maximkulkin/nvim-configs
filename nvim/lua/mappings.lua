e440554d8a23682870912e3ccc5a35fe97f192d4
Maxim Kulkin <maxim.kulkin@gmail.com>
2024-10-06 21:12:25 -0400

    Add keymaps to switch between buffers

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,3 +5,6 @@ vim.keymap.set('n', 'E', ':e <C-R>=expand("%:h")<CR>/')
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('n', '<leader>s', ':source %<CR>')
 
+vim.keymap.set('n', '<C-M-[>', ':bp<CR>', { silent = true })
+vim.keymap.set('n', '<C-M-]>', ':bn<CR>', { silent = true })
+
