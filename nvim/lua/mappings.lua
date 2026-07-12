6faa0b522086885c0644da057f5cd409f3592b35
Maxim Kulkin <maxim.kulkin@gmail.com>
2026-07-12 01:28:55 -0400

    Add some common keymaps like switching buffers or opening Nvim config

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,10 +1,22 @@
+-- Quick copy current line
 vim.keymap.set('n', 'Y', 'yy')
+-- Switch to last buffer
 vim.keymap.set('n', '<C-e>', ':b# <CR>')
-vim.keymap.set('n', 'E', ':e <C-R>=expand("%:h")<CR>/')
 
+-- Quick exit to normal mode
 vim.keymap.set('i', 'jk', '<esc>')
-vim.keymap.set('n', '<leader>s', ':source %<CR>')
 
+-- Delete current buffer
+vim.keymap.set('n', '<leader>bd', ':Bdelete<CR>')
+-- Switch buffers
+vim.keymap.set('n', '[b', ':bp<CR>', { silent = true })
+vim.keymap.set('n', ']b', ':bn<CR>', { silent = true })
 vim.keymap.set('n', '<C-M-[>', ':bp<CR>', { silent = true })
 vim.keymap.set('n', '<C-M-]>', ':bn<CR>', { silent = true })
 
+-- Quick source current (Lua?) file
+vim.keymap.set('n', '<leader>er', ':source %<CR>')
+
+-- Shortcut to edit Vim config and re-read it
+vim.keymap.set('n', '<leader>ev', ':e $MYVIMRC<CR>', { silent = true })
+vim.keymap.set('n', '<leader>es', ':source $MYVIMRC<CR>', { silent = true })
