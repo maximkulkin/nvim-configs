55914d501d2a99dc3f4803a4f0c5c6558c2aa477
Maxim Kulkin <maxim.kulkin@gmail.com>
2026-07-12 01:23:26 -0400

    Tweak Lua LSP server settings to assume LuaJIT Lua version

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -74,6 +74,11 @@ lspconfig.lua_ls.setup {
 
   settings = {
     Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
       diagnostics = {
         globals = { 'vim' },
       },
